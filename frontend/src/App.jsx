c825fddcfabbc6d42e29803d93edff9339b6dfe7
Josh Lim <justdoit.josh@gmail.com>
2024-08-27 16:03:29 +0800

    chore: testing route for BrowserRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+
+Routes
+
 function App() {
   return (
     <>
       <h1>React Hoot</h1>
+      <Routes>
+        <Route path="/test" element={<h1>Test</h1>} ></Route>
+      </Routes>
     </>
   );
 }
